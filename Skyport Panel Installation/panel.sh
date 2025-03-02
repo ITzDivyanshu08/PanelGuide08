ba6733aed361583e324e8635a944c4f66c464c61
ITzDivyanshu08 <divyanshubarthwal08@gmail.com>
2025-03-02 19:44:32 +0530

    Update panel.sh

--- Skyport Panel Installation/panel.sh
+++ Skyport Panel Installation/panel.sh
@@ -24,7 +24,7 @@ sudo apt update
 sudo apt install -y nodejs git
 
 # Step 6: Clone the repository
-git clone https://github.com/achul123/panel5
+git clone https://github.com/g-flame-oss/skyport
 
 # Step 7: Navigate to the project directory
 cd panel5
